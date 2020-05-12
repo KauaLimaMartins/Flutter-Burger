b1c8aefaa063414fa7484cb749473d870fba54c0
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-12 16:53:48 -0300

    Widgets

---
+++ lib/widgets/input.widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class Input extends StatelessWidget {
+  String label = '';
+  var controller = new MoneyMaskedTextController();
+
+  Input({
+    @required this.label,
+    @required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 140,
+          alignment: Alignment.centerRight,
+          child: Text(
+            "$label:",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 38,
+              fontFamily: 'Big Shoulders Display',
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextFormField(
+            controller: controller,
+            keyboardType: TextInputType.number,
+            cursorColor: Colors.white,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 48,
+              fontFamily: 'Big Shoulders Display',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/logo.widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 25,
+        ),
+        Image.asset(
+          "assets/images/burguer.png",
+          height: 100,
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "Burguer Comparator",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+            fontFamily: 'Roboto',
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
